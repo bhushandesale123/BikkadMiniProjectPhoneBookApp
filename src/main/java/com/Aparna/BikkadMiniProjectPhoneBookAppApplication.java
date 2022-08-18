63df1fd90de8433f04084fb605894e0e8d65178d
aparnajadhav123 <aparnajdhav5133@gmail.com>
2022-08-18 19:53:07 +0530

    BMOOBA-1: created projcet phone book app

---
+++ src/main/java/com/Aparna/BikkadMiniProjectPhoneBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.Aparna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikkadMiniProjectPhoneBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikkadMiniProjectPhoneBookAppApplication.class, args);
+	}
+
+}
